8e8bd246de0993809a5b02f8aaa8ea96321945d1
RivaldoTjio <rivaldohadiwinata@gmail.com>
2022-10-20 20:40:46 +0700

    unit test

--- app/src/main/java/com/rivaldo/tiptime/MainActivity.kt
+++ app/src/main/java/com/rivaldo/tiptime/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.StringRes
+import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -23,6 +24,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.rivaldo.tiptime.ui.theme.TipTimeTheme
 import java.text.NumberFormat
+import java.util.*
 import kotlin.math.round
 
 class MainActivity : ComponentActivity() {
@@ -42,11 +44,12 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String {
+@VisibleForTesting
+internal fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String {
     var tip = tipPercent / 100 * amount
     if (roundUp)
         tip = kotlin.math.ceil(tip)
-    return NumberFormat.getCurrencyInstance().format(tip)
+    return NumberFormat.getCurrencyInstance(Locale.US).format(tip)
 }
 
 
